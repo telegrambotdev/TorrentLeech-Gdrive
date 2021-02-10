21fb6a49bcf23de6c7a29b9edfea47fdf91cb99f
TONY <rsoorajs@gmail.com>
2021-02-10 13:10:22 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -15,11 +15,11 @@ class Config(Config):
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     RCLONE_CONFIG = """
 [DRIVE]
-type = drive
-client_id = 28dslkgjsdl-fill-your-details-apps.googleusercontent.com
-client_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc
-scope = drive
-root_folder_id =
-token = {"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczsHg6MYPSE6hXo-PJ-fill-your-details-s06KAecfw_H-tYThBtbs:20:25.430920315Z"}
-team_drive = 0AB0q-fill-your-details-sdrgsgsdUk9PVA
+type = drive\n
+client_id = 28dslkgjsdl-fill-your-details-apps.googleusercontent.com\n
+client_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc\n
+scope = drive\n
+root_folder_id = \n
+token = {"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczsHg6MYPSE6hXo-PJ-fill-your-details-s06KAecfw_H-tYThBtbs:20:25.430920315Z"} \n
+team_drive = 0AB0q-fill-your-details-sdrgsgsdUk9PVA \n
 """
